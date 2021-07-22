1ebafe5915d8f9d6d2bc1efa2dc8e385d8d507f0
Brian Sigurdson <sigurdson.brian@gmail.com>
2021-07-22 11:49:57 -0400

    updates

---
+++ nano_java_dev/0_java_fundamentals/lessons/lesson3/src/inheritanceExample/Person.java
@@ -0,0 +1,33 @@
+package inheritanceExample;
+
+public class Person {
+    private String firstName;
+    private String lastName;
+
+    public Person(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String firstName) {
+        this.lastName = firstName;
+    }
+
+    public String toString() {
+        return "Full name: " + firstName + " " + lastName;
+    }
+
+
+}
