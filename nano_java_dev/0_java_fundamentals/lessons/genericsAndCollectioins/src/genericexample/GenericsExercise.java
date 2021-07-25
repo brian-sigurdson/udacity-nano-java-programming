0fa09a47f505a62ad2017b880e8248c40097b02c
Brian Sigurdson <sigurdson.brian@gmail.com>
2021-07-25 12:07:23 -0400

    updates

---
+++ nano_java_dev/0_java_fundamentals/lessons/genericsAndCollectioins/src/genericexample/GenericsExercise.java
@@ -0,0 +1,25 @@
+package genericexample;
+
+import java.util.ArrayList;
+
+public class GenericsExercise {
+    public static void main(String[] args) {
+        ArrayList<Object> variables = new ArrayList<>();
+        Double doubleNumber = 1.5;
+        String name = "Sally";
+        Integer intNumber = 1;
+        Character letter = 'a';
+        variables.add(doubleNumber);
+        variables.add(name);
+        variables.add(intNumber);
+        variables.add(letter);
+
+        for (Object var: variables) {
+            genericMethod(var);
+        }
+    }
+
+    public static <T> void genericMethod(T variable) {
+        System.out.println(variable);
+    }
+}
