72f21f984b809248fbb41c9a1a545c3417b795c9
Brian Sigurdson <sigurdson.brian@gmail.com>
2021-07-31 19:10:24 -0400

    more java project

--- nano_java_dev/0_java_fundamentals/project/src/ui/AdminMenu.java
+++ nano_java_dev/0_java_fundamentals/project/src/ui/AdminMenu.java
@@ -11,11 +11,13 @@ package ui;
 public class AdminMenu {
 
     public void displayAdminMenu() {
+        System.out.println("--------------------------------------------------");
         System.out.println("1. See all Customers");
         System.out.println("2. See all Rooms");
         System.out.println("3. See all Reservations");
         System.out.println("4. Add a Room");
         System.out.println("5. Back to Main Menu");
+        System.out.println("--------------------------------------------------");
     }
 
     // TODO: incorporate scanner
