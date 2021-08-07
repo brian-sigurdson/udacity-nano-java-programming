f0547272a4f76bfaba977eb33a5ef762cf66aaab
Brian Sigurdson <sigurdson.brian@gmail.com>
2021-08-07 19:06:24 -0400

    leftover java from yesterday

--- nano_java_dev/0_java_fundamentals/project/src/model/FreeRoom.java
+++ nano_java_dev/0_java_fundamentals/project/src/model/FreeRoom.java
@@ -7,6 +7,14 @@ public class FreeRoom extends Room {
     Looking on the forum, I found this:  https://knowledge.udacity.com/questions/534314
     I skimmed it, but I think it corroborates my thought that it doesn't really fit in well.
 
+    This one isn't directly on FreeRoom, but read it all, because it make use of the free room class a lot to
+    explain some concepts, so perhaps I'll get a better understanding of how it is used.
+    https://knowledge.udacity.com/questions/570279
+
+    ** Actually, I'm starting to think that this really is suppose to be a "free" room, as opposed to
+    a premium or economy room.  If so, i think something more in line to an actual hotel usage, such as
+    a "premium" room, would have been a better choice.
+
     See if there are any other comments that clarify this class as well.
 
     Otherwise, I don't think it is really used.
@@ -19,6 +27,6 @@ public class FreeRoom extends Room {
     // TODO: Double check that this is in line with the example output
     @Override
     public String toString() {
-        return super.toString();
+        return "FreeRoom: " + super.toString();
     }
 }
