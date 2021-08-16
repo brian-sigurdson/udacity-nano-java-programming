67ef80a55552ac1c913bfd8036c5857bd26c105d
Brian Sigurdson <sigurdson.brian@gmail.com>
2021-08-15 20:47:50 -0400

    update forestation view

--- nano_sql/0_intro_to_sql/project/create_forestation_view.sql
+++ nano_sql/0_intro_to_sql/project/create_forestation_view.sql
@@ -11,7 +11,7 @@ select r.country_name,
        f.forest_area_sqkm / 2.59 as forest_area_sq_mi,
        la.total_area_sq_mi,
        la.total_area_sq_mi * 2.59 as total_area_sqkm,
-       round( (f.forest_area_sqkm / la.total_area_sq_mi * 100)::numeric, 2) as percent_forest
+       round( (f.forest_area_sqkm / (la.total_area_sq_mi * 2.59) * 100)::numeric, 2) as percent_forest
 from regions r
 join forest_area f on r.country_code = f.country_code
 join land_area la on f.country_code = la.country_code and f.year = la.year
