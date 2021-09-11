11dc164cc3547b8aa074eed0ad2130ef9fd36528
Brian Sigurdson <sigurdson.brian@gmail.com>
2021-09-11 19:08:34 -0400

    updates

--- 1_java_advanced/lessons/src/lesson_03/exer_11/MergeShards.java
+++ 1_java_advanced/lessons/src/lesson_03/exer_11/MergeShards.java
@@ -1,13 +1,16 @@
 package lesson_03.exer_11;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
+import java.nio.Buffer;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
+import java.util.PriorityQueue;
 import java.util.stream.Collectors;
 
 public final class MergeShards {
@@ -18,41 +21,72 @@ public final class MergeShards {
         }
 
         List<Path> inputs = Files.walk(Path.of(args[0]), 1).skip(1).collect(Collectors.toList());
-
-//        inputs.forEach(System.out::println);
-//        if (true) return;
-
         List<BufferedReader> readers = new ArrayList<>(inputs.size());
-        Path outputPath = Path.of(args[1]);
+        Path pathToOutputFile = Path.of(args[1]);
 
-//        BufferedReader reader = Files.newBufferedReader(input, StandardCharsets.UTF_8);
-
-
-        // i guess this is what it needs?
         try {
-            inputs.forEach((n) -> {
+            // create one reader for each shard file
+            inputs.forEach(n -> {
                 try {
                     readers.add(Files.newBufferedReader(n, StandardCharsets.UTF_8));
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
             });
+
+            // create a priority queue with initial capacity
+            PriorityQueue<WordEntry> priorityQueue = new PriorityQueue<>(inputs.size());
+
+            String line;
+            try (BufferedWriter bufferedWriter = Files.newBufferedWriter(pathToOutputFile, StandardCharsets.UTF_8))
+            {
+                // iterate over the input readers and read one word from the file
+                for (int i = 0; i < readers.size(); i++) {
+                    line = readers.get(i).readLine();
+                    // process the line
+                    MergeShards.processLine(priorityQueue, line, readers.get(i));
+                }
+
+                // while the queue has elements to write
+                while (!priorityQueue.isEmpty()) {
+                    // remove the highest priority element from the queue
+                    WordEntry wordEntry = priorityQueue.poll();
+                    // write it to the output file
+                    bufferedWriter.write(wordEntry.word + System.lineSeparator());
+                    // use the reader associated with the word just written to read in a word from the same file
+                    line = wordEntry.reader.readLine();
+                    // process line
+                    MergeShards.processLine(priorityQueue, line, wordEntry.reader);
+                }
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
         } finally {
-            
+            // close the readers
+            readers.forEach(n -> {
+                try {
+                    n.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            });
         }
+    }
 
-
-
-        // TODO: Inside a try-finally block, create the List of BufferedReaders: one for each "input"
-        //       Path. Without modifying the shard files, merge them together into a single text file
-        //       whose location is specified by the "outputPath".
-        //
-        //       To do this, you should put words in a PriorityQueue<WordEntry>, but make sure the
-        //       priority queue never contains more entries than there are input files. The whole point
-        //       of doing a distributed merge sort is that there are too many words to fit into memory!
-        //
-        //       In the "finally" part of the try-finally block, make sure to close all the
-        //       BufferedReaders.
+    private static void processLine(PriorityQueue<WordEntry> priorityQueue, String line, BufferedReader bufferedReader) {
+        try {
+            if (line == null) {
+                // file is empty
+                bufferedReader.close();
+            } else {
+                // add it to the queue
+                priorityQueue.add(new WordEntry(line, bufferedReader));
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     private static final class WordEntry implements Comparable<WordEntry> {
